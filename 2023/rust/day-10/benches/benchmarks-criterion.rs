4805e9444f56c9c544f8137d04e8cd1e1134d512
K Sai Akshit <ksaiakshit@gmail.com>
2023-12-21 14:56:51 +0530

    Day 09 & 10

--- 2023/rust/day-10/benches/benchmarks-criterion.rs
+++ 2023/rust/day-10/benches/benchmarks-criterion.rs
@@ -1,12 +1,12 @@
 use criterion::{
     criterion_group, criterion_main, Criterion,
 };
-use {{crate_name}}::*;
+use day_10::*;
 
 fn criterion_benchmark_part1(c: &mut Criterion) {
     let input = include_str!("../input1.txt");
 
-    let mut group = c.benchmark_group("{{crate_name}}::part1");
+    let mut group = c.benchmark_group("day_10::part1");
     group.bench_with_input("part1", input, |b, input| {
         b.iter(|| part1::process(input))
     });
@@ -17,7 +17,7 @@ fn criterion_benchmark_part1(c: &mut Criterion) {
 fn criterion_benchmark_part2(c: &mut Criterion) {
     let input = include_str!("../input2.txt");
 
-    let mut group = c.benchmark_group("{{crate_name}}::part2");
+    let mut group = c.benchmark_group("day_10::part2");
     group.bench_with_input("part2", input, |b, input| {
         b.iter(|| part2::process(input))
     });
